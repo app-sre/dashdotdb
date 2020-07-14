5fc20cd424ac767c65d7153b3ce8d4377485be62
Amador Pahim <apahim@redhat.com>
2020-07-14 10:01:25 +0100

    Remove DB Indexes
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- dashdotdb/models/imagemanifestvuln.py
+++ dashdotdb/models/imagemanifestvuln.py
@@ -15,7 +15,7 @@ class Pod(db.Model):
     __tablename__ = 'pod'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), index=True, unique=False)
+    name = db.Column(db.String(256), unique=False)
     namespace_id = db.Column(db.Integer, db.ForeignKey('namespace.id'))
     image_id = db.Column(db.Integer, db.ForeignKey('image.id'))
     token_id = db.Column(db.Integer, db.ForeignKey('token.id'))
@@ -26,7 +26,7 @@ class Namespace(db.Model):
     __tablename__ = 'namespace'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=False)
+    name = db.Column(db.String(64), unique=False)
     cluster_id = db.Column(db.Integer, db.ForeignKey('cluster.id'))
     pods = db.relationship('Pod', backref='namespace')
 
@@ -36,7 +36,7 @@ class Cluster(db.Model):
     __tablename__ = 'cluster'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(64), unique=True)
     namespaces = db.relationship('Namespace', backref='cluster')
 
 
@@ -55,8 +55,8 @@ class Image(db.Model):
     __tablename__ = 'image'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=False)
-    manifest = db.Column(db.String(1000), index=True, unique=False)
+    name = db.Column(db.String(64), unique=False)
+    manifest = db.Column(db.String(1000), unique=False)
     features = db.relationship('Feature', secondary='imagefeature')
     pods = db.relationship('Pod', backref='image')
 
@@ -66,10 +66,10 @@ class Feature(db.Model):
     __tablename__ = 'feature'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=False)
-    namespacename = db.Column(db.String(64), index=True, unique=False)
-    version = db.Column(db.String(64), index=True, unique=False)
-    versionformat = db.Column(db.String(64), index=True, unique=False)
+    name = db.Column(db.String(64), unique=False)
+    namespacename = db.Column(db.String(64), unique=False)
+    version = db.Column(db.String(64), unique=False)
+    versionformat = db.Column(db.String(64), unique=False)
     images = db.relationship('Image', secondary='imagefeature')
     vulnerabilities = db.relationship('Vulnerability', backref='feature')
 
@@ -79,10 +79,10 @@ class Vulnerability(db.Model):
     __tablename__ = 'vulnerability'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=False)
-    description = db.Column(db.String(10000), index=True, unique=False)
-    fixedby = db.Column(db.String(10000), index=True, unique=False)
-    link = db.Column(db.String(10000), index=True, unique=False)
+    name = db.Column(db.String(64), unique=False)
+    description = db.Column(db.String(10000), unique=False)
+    fixedby = db.Column(db.String(10000), unique=False)
+    link = db.Column(db.String(10000), unique=False)
     severity_id = db.Column(db.Integer, db.ForeignKey('severity.id'))
     feature_id = db.Column(db.Integer, db.ForeignKey('feature.id'))
 
@@ -92,5 +92,5 @@ class Severity(db.Model):
     __tablename__ = 'severity'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(64), unique=True)
     vulnerabilities = db.relationship('Vulnerability', backref='severity')
