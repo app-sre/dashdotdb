9be17159a66cd046f363dafe8d6aef7aaf31f52a
Amador Pahim <apahim@redhat.com>
2020-07-20 09:43:36 +0100

    Reducing councurrent threads and increasing workers
    Each worker is a separate process. They are good for parallelism.
    On the other hand, threads are good for multitasking, but too much threads
    will acknoledge the connection without being able to serve it.
    
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 # Init the database
 FLASK_APP=dashdotdb flask db upgrade
 # Run the app
-gunicorn dashdotdb:app --workers 1 --threads 8 --bind 0.0.0.0:8080
+gunicorn dashdotdb:app --workers 2 --threads 2 --bind 0.0.0.0:8080
