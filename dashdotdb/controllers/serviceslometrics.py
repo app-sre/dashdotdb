337b4b1f0f254f89a64c7e542d23629bdfc7f30c
Violet Yao <vyao@redhat.com>
2021-03-17 23:02:43 -0700

    post & get first draft

---
+++ dashdotdb/controllers/serviceslometrics.py
@@ -0,0 +1,12 @@
+from dashdotdb.services.serviceslometrics import ServiceSLOMetrics
+
+
+def post(cluster, body):
+    imv = ServiceSLOMetrics(cluster=cluster)
+    imv.insert(slo=body)
+    return 'ok'
+
+def search(cluster, namespace, sli_type):
+    imv = ServiceSLOMetrics(cluster, namespace, sli_type)
+    return imv.get_slometrics()
+
---
+++ dashdotdb/services/serviceslometrics.py
@@ -0,0 +1,139 @@
+import logging
+
+from datetime import datetime
+from datetime import timedelta
+
+from sqlalchemy import func
+
+from dashdotdb.models.dashdotdb import db
+from dashdotdb.models.dashdotdb import Token
+from dashdotdb.models.dashdotdb import Service
+from dashdotdb.models.dashdotdb import Cluster
+from dashdotdb.models.dashdotdb import Namespace
+from dashdotdb.models.dashdotdb import ServiceSLO
+from dashdotdb.models.dashdotdb import SLIType
+
+
+class ServiceSLOMetrics:
+    def __init__(self, cluster=None, namespace=None, sli_type=None):
+        self.log = logging.getLogger()
+
+        # self.service = service
+        self.cluster = cluster
+        self.namespace = namespace
+        self.sli_type = sli_type
+        # self.sloname = sloname
+        # self.value = value
+        # self.target = target 
+
+    def insert(self, slo):
+
+        expire = datetime.now() - timedelta(minutes=60)
+        db_token = db.session.query(Token) \
+            .filter(Token.timestamp > expire).first()
+        if db_token is None:
+            db.session.add(Token(timestamp=datetime.now()))
+            db.session.commit()
+            self.log.info('token created')
+        db_token = db.session.query(Token) \
+            .filter(Token.timestamp > expire).first()
+
+        service_name = slo['service']['name']
+        db_service = db.session.query(Service) \
+            .filter_by(name=service_name).first()
+        if db_service is None:
+            db.session.add(Service(name=service_name))
+            db.session.commit()
+            self.log.info('service %s created', service_name)
+        db_service = db.session.query(Service) \
+            .filter_by(name=service_name).first()
+
+        cluster_name = slo['cluster']['name']
+        db_cluster = db.session.query(Cluster) \
+            .filter_by(name=cluster_name).first()
+        if db_cluster is None:
+            db.session.add(Cluster(name=cluster_name))
+            db.session.commit()
+            self.log.info('cluster %s created', cluster_name)
+        db_cluster = db.session.query(Cluster) \
+            .filter_by(name=cluster_name).first()
+
+        namespace_name = slo['namespace']['name']
+        db_namespace = db.session.query(Namespace) \
+            .filter_by(name=namespace_name, cluster_id=db_cluster.id).first()
+        if db_namespace is None:
+            db.session.add(Namespace(name=namespace_name,
+                                     cluster_id=db_cluster.id))
+            db.session.commit()
+            self.log.info('namespace %s created', namespace_name)
+        db_namespace = db.session.query(Namespace) \
+            .filter_by(name=namespace_name, cluster_id=db_cluster.id).first()
+
+        slitype_name = slo['SLIType']
+        db_slitype = db.session.query(SLIType) \
+            .filter_by(name=slitype_name).first()
+        if db_slitype is None:
+            db.session.add(SLIType(name=slitype_name))
+            db.session.commit()
+            self.log.info('slitype %s created', slitype_name)
+        db_slitype = db.session.query(SLIType) \
+            .filter_by(name=slitype_name).first()
+
+        db_serviceslo = db.session.query(ServiceSLO) \
+                    .filter_by(service_id=db_service.id,
+                               cluster_id=db_cluster.id,
+                               namespace_id=db_namespace.id,
+                               slitype_id=db_slitype.id
+                    ).first()
+        if db_serviceslo is None:
+            db.session.add(ServiceSLO(service_id=db_service.id,
+                                    cluster_id=db_cluster.id,
+                                    namespace_id=db_namespace.id,
+                                    slitype_id=db_slitype.id))
+            db.session.commit()
+            self.log.info('ServiceSLO %s created ', slo['name'])
+        db_serviceslo = db.session.query(ServiceSLO) \
+                    .filter_by(service_id=db_service.id,
+                               cluster_id=db_cluster.id,
+                               namespace_id=db_namespace.id,
+                               slitype_id=db_slitype.id
+                    ).first()
+        db_serviceslo.name = slo['name']
+        db_serviceslo.value = slo['value']
+        db_serviceslo.target = slo['target']
+        db.session.commit()
+        self.log.info('ServiceSLO %s updated ', slo['name'])
+
+    def get_slometrics(self):
+        # token = db.session.query(Token) \
+        #     .filter(ServiceSLO.token_id == Token.id,
+        #             ServiceSLO.namespace_id == Namespace.id,
+        #             Namespace.cluster_id == Cluster.id,
+        #             Cluster.name == self.cluster) \
+        #     .order_by(Token.timestamp.desc()) \
+        #     .limit(1) \
+        #     .first()
+        token = db.session.query(Token).first()
+        print(f"token_id is: {token.id}")
+        # print(f"self.cluster is: {self.cluster}")
+        if token is None:
+            return []
+
+        # ServiceSLO.token_id == token.id, 
+        serviceslo = db.session.query(ServiceSLO) \
+            .filter(ServiceSLO.slitype_id == SLIType.id,
+                    SLIType.name == self.sli_type,
+                    ServiceSLO.namespace_id == Namespace.id,
+                    Namespace.name == self.namespace,
+                    Namespace.cluster_id == Cluster.id,
+                    Cluster.name == self.cluster).first()
+        # serviceslo = db.session.query(ServiceSLO).first()
+        
+        result = {
+            'name': serviceslo.name,
+            'value': serviceslo.value,
+            'target': serviceslo.target
+        }
+
+        return result
+
