019acb56317e2fb9ba3ee445dd8d47936854c057
Jaime Melis <jmelis@redhat.com>
2023-09-12 16:50:37 +0200

    more info in conflict error message

--- dashdotdb/controllers/dora.py
+++ dashdotdb/controllers/dora.py
@@ -3,13 +3,15 @@ from sqlalchemy import exc
 
 from dashdotdb.services.dora import DORA
 
+
 def post(user, body):
     try:
         msg, code = DORA().insert(token=user, manifest=body)
     except exc.IntegrityError as e:
         if isinstance(e.orig, UniqueViolation):
-            return "UniqueViolation. Data already exists in DB.", 409
-        raise(e)
+            errmsg = f"UniqueViolation. Data already exists in DB: {e.orig.pgerror}"
+            return errmsg, 409
+        raise (e)
     return msg, code
 
 
