e9fafe799e21790765ee5cebe070995ec9f2b97b
Jaime Melis <jmelis@redhat.com>
2023-09-13 10:18:17 +0200

    transaction resiliency

--- dashdotdb/controllers/dora.py
+++ dashdotdb/controllers/dora.py
@@ -1,18 +1,15 @@
-from psycopg2.errors import UniqueViolation
-from sqlalchemy import exc
-
 from dashdotdb.services.dora import DORA
 
 
 def post(user, body):
-    try:
-        msg, code = DORA().insert(token=user, manifest=body)
-    except exc.IntegrityError as e:
-        if isinstance(e.orig, UniqueViolation):
-            errmsg = f"UniqueViolation. Data already exists in DB: {e.orig.pgerror}"
-            return errmsg, 409
-        raise (e)
-    return msg, code
+    stats = DORA().insert(token=user, manifest=body)
+
+    if stats['committed']:
+        code = 201
+    else:
+        code = 400
+
+    return stats, code
 
 
 def latest_deployment(app_name, env_name):
--- dashdotdb/services/dora.py
+++ dashdotdb/services/dora.py
@@ -1,21 +1,11 @@
 import logging
 import datetime
 
+from psycopg2.errors import UniqueViolation
+from sqlalchemy import exc
 
-# from sqlalchemy import func
-
-from dashdotdb.models.dashdotdb import (
-    db,
-    Token,
-    DataTypes,
-    # LatestTokens,
-    DORADeployment,
-    DORACommit
-)
-
-
-from dashdotdb.controllers.token import (TOKEN_NOT_FOUND_CODE,
-                                         TOKEN_NOT_FOUND_MSG)
+from dashdotdb.models.dashdotdb import db, Token, DataTypes, DORADeployment, DORACommit
+from dashdotdb.controllers.token import TOKEN_NOT_FOUND_CODE, TOKEN_NOT_FOUND_MSG
 
 
 class DORA:
@@ -23,63 +13,95 @@ class DORA:
         self.log = logging.getLogger()
 
     def insert(self, token, manifest):
-        db_token = db.session.query(Token) \
-            .filter(Token.uuid == token,
-                    Token.data_type == DataTypes.DORADataType).first()
+        db_token = (
+            db.session.query(Token)
+            .filter(Token.uuid == token, Token.data_type == DataTypes.DORADataType)
+            .first()
+        )
 
         if db_token is None:
-            self.log.error(
-                'skipping validation: %s %s', TOKEN_NOT_FOUND_MSG, token)
+            self.log.error("skipping validation: %s %s", TOKEN_NOT_FOUND_MSG, token)
             return TOKEN_NOT_FOUND_MSG, TOKEN_NOT_FOUND_CODE
 
+        stats = {
+            "committed": [],
+            "duplicate": [],
+            "error": [],
+        }
+
         for dep_data in manifest["deployments"]:
-            finish_timestamp = datetime.datetime.fromisoformat(
-                dep_data['finish_timestamp'])
+            # start transaction - we want to do an atomic
+            # transaction of deployment and commits
+            db.session.begin_nested()
 
-            # finish_timestamp and timestamp may come offset-naive or offset-aware, and
-            # they can't be subtracted if they are different. Converting always
-            # to offset-aware. If no TZ data is provided, we assume UTC.
-            if finish_timestamp.tzinfo is None:
-                finish_timestamp = finish_timestamp.replace(
-                    tzinfo=datetime.timezone.utc)
-
-            deployment = DORADeployment(
-                token_id=db_token.id,
-                finish_timestamp=finish_timestamp,
-                trigger_reason=dep_data['trigger_reason'],
-                app_name=dep_data['app_name'],
-                env_name=dep_data['env_name'],
-                pipeline=dep_data['pipeline'],
+            dep_short_name = (
+                "app_name={},env_name={},pipeline={},trigger_reason={}".format(
+                    dep_data["app_name"],
+                    dep_data["env_name"],
+                    dep_data["pipeline"],
+                    dep_data["trigger_reason"],
+                )
             )
-            db.session.add(deployment)
 
             try:
-                for commit_data in dep_data["commits"]:
-                    timestamp = datetime.datetime.fromisoformat(commit_data['timestamp'])
-                    if timestamp.tzinfo is None:
-                        timestamp = timestamp.replace(tzinfo=datetime.timezone.utc)
+                finish_timestamp = datetime.datetime.fromisoformat(
+                    dep_data["finish_timestamp"]
+                )
 
-                    lttc = finish_timestamp - timestamp
+                # finish_timestamp and timestamp may come offset-naive or offset-aware,
+                # and they can't be subtracted if they are different. Converting always
+                # to offset-aware. If no TZ data is provided, we assume UTC.
+                if finish_timestamp.tzinfo is None:
+                    finish_timestamp = finish_timestamp.replace(
+                        tzinfo=datetime.timezone.utc
+                    )
+
+                deployment = DORADeployment(
+                    token_id=db_token.id,
+                    finish_timestamp=finish_timestamp,
+                    trigger_reason=dep_data["trigger_reason"],
+                    app_name=dep_data["app_name"],
+                    env_name=dep_data["env_name"],
+                    pipeline=dep_data["pipeline"],
+                )
+                db.session.add(deployment)
+                db.session.commit()
+
+                for commit_data in dep_data["commits"]:
                     commit = DORACommit(
                         deployment_id=deployment.id,
-                        timestamp=datetime.datetime.fromisoformat(commit_data['timestamp']),
-                        revision=commit_data['revision'],
-                        repo=commit_data['repo'],
-                        lttc=lttc,
+                        timestamp=datetime.datetime.fromisoformat(
+                            commit_data["timestamp"]
+                        ),
+                        revision=commit_data["revision"],
+                        repo=commit_data["repo"],
+                        lttc=datetime.timedelta(seconds=commit_data["lttc"]),
                     )
                     db.session.add(commit)
-
                 db.session.commit()
-            except:
-                # If any of the 'commits' fail, we throw away everything,
-                # including the 'deployment'
-                db.session.rollback()
-                return "bad request", 400
+            except Exception as e:
+                if isinstance(e, exc.IntegrityError) and isinstance(
+                    e.orig, UniqueViolation
+                ):
+                    stats["duplicate"].append(dep_short_name)
+                    self.log.info("DUPLICATE deployment: %s", dep_short_name)
+                else:
+                    stats["error"].append(dep_short_name)
+                    self.log.error("ERROR deployment: %s - %s", dep_short_name, e)
 
-        return "ok", 200
+                db.session.rollback()
+            else:
+                stats["committed"].append(dep_short_name)
+
+        return stats
 
     def get_latest_deployment(self, app_name, env_name):
-        return db.session.query(DORADeployment).filter_by(
-            app_name=app_name,
-            env_name=env_name,
-        ).order_by(DORADeployment.finish_timestamp.desc()).first()
+        return (
+            db.session.query(DORADeployment)
+            .filter_by(
+                app_name=app_name,
+                env_name=env_name,
+            )
+            .order_by(DORADeployment.finish_timestamp.desc())
+            .first()
+        )
