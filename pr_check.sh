05528f6be4428b0f834d7559055245b124a73d11
Ryan Wallace <rywallac@redhat.com>
2024-10-10 14:26:36 -0400

    update postgres image to use Ubi, add healthchecks
    add tests to pr-check script
    
    use sleep command instead of wait
    
    use dynamic ports for podman execution
    
    use awk to extract port
    
    fix awk command
    
    move things back to one pr_check.sh file, force-recreate option,
    explicit return value using global var
    
    use dashdotdb network
    
    display logs
    
    force restarts
    
    increase sleep
    
    remove tty options
    
    use raw podman commands rather than compose
    
    detached containers
    
    wait til container is created before connecting network
    
    hardcoded resource names
    
    fix podman args
    
    port forwarding for pg connection
    
    lets see why our container isnt running
    
    dont use localhost
    
    trim IP
    
    run detached again
    
    fix return
    
    silent curl

--- pr_check.sh
+++ pr_check.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
-set -exvo pipefail
+set -xvo pipefail
+
+# $1: api_type
+# $2: web app port
+test_data() {
+    TOKEN=$(curl --silent http://localhost:$2/api/v1/token?scope=$1 | sed 's/"//g')
+    POST_RES=$(curl --silent -X POST --header "Content-Type: application/json" --header "X-Auth: $TOKEN" --data @examples/$1.json http://localhost:$2/api/v1/$1/app-sre-prod-01)
+    if [[ ! $POST_RES =~ "ok" ]]; then
+        echo "test of $1 api failed, returning '$POST_RES' when 'ok' was expected"
+        RETURN=1
+    fi
+    curl --silent --request DELETE "http://localhost:$2/api/v1/token/$TOKEN?scope=$1"
+}
 
 make ci
+RETURN=$?
+
+IP=$(hostname -I | tr -d '[:blank:]')
+
+DB_CONTAINER=dashdotdb-pg
+APP_CONTAINER=dashdotdb-app
+
+podman run --rm -d -e POSTGRESQL_PASSWORD=postgres -e POSTGRESQL_USER=dashdotdb -e POSTGRESQL_DATABASE=dashdotdb --health-cmd "pg_isready -U dashdotdb -d dashdotdb" --name dashdotdb-pg -p 5432 registry.redhat.io/rhel9/postgresql-15
+podman wait --condition=healthy $DB_CONTAINER
+PG_PORT=$(podman port "${DB_CONTAINER}" | awk -F: '{print $2}')
+
+podman run --rm -d -e DASHDOTDB_DATABASE_URL=postgresql://dashdotdb:postgres@$IP:$PG_PORT/dashdotdb -p 8080 --health-cmd "curl -f http://localhost:8080/api/healthz/ready" --name $APP_CONTAINER app-sre-dashdotdb:do-not-use
+podman wait --condition=healthy $APP_CONTAINER
+
+
+WEB_PORT=$(podman port "${APP_CONTAINER}" | awk -F: '{print $2}')
+
+test_data imagemanifestvuln "$WEB_PORT"
+test_data serviceslometrics "$WEB_PORT"
+test_data deploymentvalidation "$WEB_PORT"
+
+podman stop $APP_CONTAINER $DB_CONTAINER
+
+exit $RETURN
