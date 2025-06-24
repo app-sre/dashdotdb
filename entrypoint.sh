466ae1411d43a191824cf177bd35ddfa138d7323
Lisa Seelye <18159+lisa@users.noreply.github.com>
2025-06-24 08:33:19 -0400

    APPSRE-11100: Fixup dashdotdb uv (#144)
    * APPSRE-11100: Fixup dashdotdb uv
    
    The previous commits (865b64ba and cab5fc9d) didn't quite get dashdotdb working in stage because of OCP runtime restrictions that were not evident in my local development and testing. This commit, on the other hand, should be expected to work because the `prod` image will not rely on `uv` to run flask and gunicorn.
    
    With much credit to Di for his keen reviews.
    
    Signed-off-by: Lisa Seelye <lisa@users.noreply.github.com>
    Co-authored-by: Lisa Seelye <lisa@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Init the database
-FLASK_APP=dashdotdb uv run --no-group dev flask db upgrade
+FLASK_APP=dashdotdb flask db upgrade
 # Run the app
-uv run --no-group dev gunicorn dashdotdb:app --workers 2 --threads 2 --bind 0.0.0.0:8080
+gunicorn dashdotdb:app --workers 2 --threads 2 --bind 0.0.0.0:8080
